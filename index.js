1451917e752ead3948be335cb172963a3c42d83c
Geordy <ggeordymm@gmail.com>
2025-01-14 19:28:05 -0500

    Añadido para uso en api

--- index.js
+++ index.js
@@ -1,78 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const { google } = require('googleapis');
 const fs = require('fs');
 const path = require('path');
 var Minizip = require('minizip-asm.js');
+const mysqldump = require('mysqldump');
 
 // Ruta a tu archivo de credenciales JSON descargado
 const KEYFILEPATH = path.join(__dirname, 'google-credentials.json');
 const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
-const FOLDER_ID = '1j3OT0ymhzdOsX6HziW01-lZSdoHkwMbe';
+const driveService = google.drive({ version: 'v3', auth: new google.auth.GoogleAuth({ keyFile: KEYFILEPATH, scopes: SCOPES }) });
 
-const FILE_TO_UPLOAD_PATH = 'archivo.txt';;
+const app = express();
 
-const FILE_UPLOADED_NAME = 'archivo-subido.txt';
+// Configurar el middleware para parsear las solicitudes JSON
+app.use(bodyParser.json());
 
-// Autenticación con cuenta de servicio
-const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
-    scopes: SCOPES,
-});
-
-// Inicializar el cliente de Google Drive
-const driveService = google.drive({ version: 'v3', auth });
-
-
-async function uploadFile(filePath, fileName, folderKey) {
-    try {
-
-        const fileMetadata = {
-            name: fileName,
-            parents: [folderKey],
-        };
-
-        const media = {
-            mimeType: 'text/plain',
-            body: fs.createReadStream(filePath),
-        };
-
-        const response = await driveService.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id',
-        });
-
-        console.log('Archivo subido con ID:', response.data.id);
-        console.log('Nombre del archivo subido:', fileName);  // Imprime el nombre del archivo subido
-    } catch (error) {
-        console.error('Error al subir el archivo:', error);
-    }
-}
+// Función para realizar el backup de la base de datos
 async function makeDatabaseBackup(databaseUrl, databaseUser, databasePassword, databaseName, backupFileName, backupPassword) {
-    console.log("DATBASE URL: " + databaseUrl);
-    console.log("DATBASE URL: " + databaseUser);
-    console.log("DATBASE URL: " + databasePassword);
-
-    const fs = require('fs');
-    const path = require('path');
-    const mysqldump = require('mysqldump');
-
     try {
         const tmpDir = path.join(__dirname, 'tmp');
+        if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir, { recursive: true });
 
-        // Crear el directorio temporal si no existe
-        if (!fs.existsSync(tmpDir)) {
-            fs.mkdirSync(tmpDir, { recursive: true });
-        }
-
-        // Formatear la fecha para el nombre del archivo
         const currentDate = new Date();
         const formattedDate = currentDate.toISOString().replace(/:/g, '-').replace('T', '_').split('.')[0];
-
-        // Utilizar el nombre de archivo proporcionado por el usuario
         const filename = `${backupFileName}_${formattedDate}.sql`;
         const filePath = path.join(tmpDir, filename);
 
-        // Configuración de mysqldump
+        console.log("DATABSE URL + " + databaseUrl);
+
         const config = {
             connection: {
                 host: databaseUrl,
@@ -83,122 +39,84 @@ async function makeDatabaseBackup(databaseUrl, databaseUser, databasePassword, d
             dumpToFile: filePath
         };
 
-        // Realizar el backup
         await mysqldump(config);
-
-        console.log(`✅ Backup realizado exitosamente: ${filePath}`);
+        console.log(`Backup realizado exitosamente: ${filePath}`);
         return filePath;
     } catch (error) {
-        console.error('❌ Error al realizar el backup:', error);
+        console.error('Error al realizar el backup:', error);
         throw error;
     }
 }
 
+// Función para comprimir el archivo en un ZIP
 async function compressFileToZip(filePath, passwordCompression) {
     try {
-        // Crear un directorio temporal
         const tmpDir = path.join(__dirname, 'tmp');
-        if (!fs.existsSync(tmpDir)) {
-            fs.mkdirSync(tmpDir, { recursive: true });
-        }
+        if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir, { recursive: true });
 
-        // Crear un nombre para el archivo zip
         const zipFileName = path.basename(filePath) + '.zip';
         const zipFilePath = path.join(tmpDir, zipFileName);
 
-        // Leer el archivo a comprimir
         const fileBuffer = fs.readFileSync(filePath);
-
-        // Crear una instancia de Minizip
         const mz = new Minizip();
 
-        // Agregar el archivo con la contraseña
         mz.append(path.basename(filePath), fileBuffer, { password: passwordCompression });
 
-        // Obtener el buffer ZIP comprimido
         const zipBuffer = mz.zip();
-
-        // Escribir el archivo comprimido en el disco
         fs.writeFileSync(zipFilePath, zipBuffer);
 
         console.log(`Archivo comprimido con éxito en: ${zipFilePath}`);
         return zipFilePath;
     } catch (error) {
-        console.error("Error al comprimir el archivo:", error);
+        console.error('Error al comprimir el archivo:', error);
+        throw error;
     }
 }
 
-async function index() {
-    const yargs = require('yargs');
+// Función para subir el archivo a Google Drive
+async function uploadFile(filePath, fileName, folderKey) {
+    try {
+        const fileMetadata = { name: fileName, parents: [folderKey] };
+        const media = { mimeType: 'application/zip', body: fs.createReadStream(filePath) };
 
-    // Usar yargs para leer los argumentos
-    const argv = yargs
-        .option('databaseUrl', {
-            alias: 'u',
-            description: 'URL de la base de datos',
-            type: 'string',
-            demandOption: true,  // Hace que este parámetro sea obligatorio
-        })
-        .option('databaseUser', {
-            alias: 'user',
-            description: 'Usuario de la base de datos',
-            type: 'string',
-            demandOption: true,
-        })
-        .option('databasePassword', {
-            alias: 'password',
-            description: 'Contraseña de la base de datos',
-            type: 'string',
-            default: '',  // Valor por defecto vacío
-        })
-        .option('databaseName', {
-            alias: 'name',
-            description: 'Nombre de la base de datos',
-            type: 'string',
-            demandOption: true,
-        })
-        .option('backupFileName', {
-            alias: 'file',
-            description: 'Nombre del archivo de backup',
-            type: 'string',
-            demandOption: true,
-        })
-        .option('backupPassword', {
-            alias: 'backup-pass',
-            description: 'Contraseña para el backup',
-            type: 'string',
-            demandOption: true,
-        })
-        .option('folderKey', {
-            alias: 'folder',
-            description: 'Clave de la carpeta en la nube',
-            type: 'string',
-            demandOption: true,
-        })
-        .help()
-        .alias('help', 'h')
-        .argv;
+        const response = await driveService.files.create({
+            resource: fileMetadata,
+            media: media,
+            fields: 'id',
+        });
+
+        console.log('Archivo subido con ID:', response.data.id);
+        console.log('Nombre del archivo subido:', fileName);
+    } catch (error) {
+        console.error('Error al subir el archivo:', error);
+        throw error;
+    }
+}
+
+// Endpoint para ejecutar el proceso de backup y subida
+app.post('/backup', async (req, res) => {
+    const { databaseUrl, databaseUser, databasePassword, databaseName, backupFileName, backupPassword, folderKey } = req.body;
 
     try {
-        // Usar los valores obtenidos de los argumentos
-        const { databaseUrl, databaseUser, databasePassword, databaseName, backupFileName, backupPassword, folderKey } = argv;
-
         const backupFilePath = await makeDatabaseBackup(databaseUrl, databaseUser, databasePassword, databaseName, backupFileName, backupPassword);
         const backupCompressedFilePath = await compressFileToZip(backupFilePath, backupPassword);
-        console.log(backupCompressedFilePath);
+        console.log('Backup comprimido:', backupCompressedFilePath);
 
-        console.log(`Backup generado en: ${backupCompressedFilePath}`);
         await uploadFile(backupCompressedFilePath, backupFileName, folderKey);
 
-        // Si todo salió bien, retornar 0
-        process.exit(0); 
+        res.status(200).send('Backup realizado y subido con éxito');
     } catch (error) {
-        console.error("Error durante la ejecución:", error);
-
-        // Si hubo un error, retornar 1
-        process.exit(1); 
+        console.error('Error durante el proceso:', error);
+        res.status(500).send('Error durante el proceso de backup y subida');
     }
-}
+});
 
+app.get('/health', async (req, res) => {
+        res.status(200).send('Servicio activo');
+});
 
-index();
+// Iniciar el servidor en el puerto 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor API escuchando en http://localhost:${PORT}`);
+});
